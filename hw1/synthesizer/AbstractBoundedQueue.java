74bc517da86d5d0fdc1b069ef1d74355a875c166
MathPlus5_statistic_sjtu2022 <umr-sbh@sjtu.edu.cn>
2025-07-21 21:16:19 +0800

    completed hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,39 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        if (fillCount() == capacity()) {
+            return true;
+        }
+        return false;
+    }
+
+    public abstract T peek();
+
+    public abstract T dequeue();
+
+    public abstract void enqueue(T x);
+
+}
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,11 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +22,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +36,12 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        fillCount += 1;
+        last = (last + 1) % capacity;
     }
 
     /**
@@ -38,7 +50,15 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T x = rb[first];
+        rb[first] = null;
+        fillCount -= 1;
+        first = (first + 1) % capacity;
+        return x;
     }
 
     /**
@@ -46,7 +66,42 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer is empty");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new RingBufferIterator();
+    }
+
+    // Private inner class implementing Iterator<T>
+    private class RingBufferIterator implements Iterator<T> {
+        private int currentPos; // Current position in the iteration
+        private int remaining;  // Number of elements left to iterate
+
+        public RingBufferIterator() {
+            currentPos = first;
+            remaining = last;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return remaining > 0;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new RuntimeException("Ring buffer underflow");
+            }
+            T item = rb[currentPos];
+            currentPos = (currentPos + 1) % rb.length; // Wrap around if needed
+            remaining--;
+            return item;
+        }
+    }
 }
